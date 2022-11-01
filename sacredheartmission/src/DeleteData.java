b29c54c67e6fa1724a4d1afbcc336e0dbc8ebf9b
joe <santayjoseph@gmail.com>
2022-11-01 03:03:50 -0500

    completed deleteData class

---
+++ sacredheartmission/src/DeleteData.java
@@ -0,0 +1,97 @@
+import java.sql.*;
+
+public class DeleteData {
+    private int id;
+    private String table, userQuery, selectQuery, deletedData;
+
+    // set the public id for deleting data
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    // set the table to table user wants to delete data from
+    public void setTable(String table) {
+        this.table = table;
+    }
+
+    public String getTable() {
+        return table;
+    }
+
+    // create the user query
+    public void setUserQuery(String table, int id) {
+        this.userQuery = String.format("DELETE FROM %s WHERE id = %d", table, id);
+    }
+
+    public String getUserQuery() {
+        return userQuery;
+    }
+
+    // select the data and store it before deleting it
+    public void setSelectQuery(String table, int id) {
+        this.selectQuery = String.format("SELECT * FROM %s WHERE id = %d", table, id);
+    }
+
+    public String getSelectQuery() {
+        return selectQuery;
+    }
+
+    // store select query in variable to display to user
+    public void setDeletedData(String data) {
+        this.deletedData = data;
+    }
+
+    public String getDeletedData() {
+        return deletedData;
+    }
+
+    // create constructor for user to delete data with
+    public DeleteData(int id, String table) {
+        setId(id);
+        setTable(table);
+        setUserQuery(table, id);
+        setSelectQuery(table, id);
+    }
+
+    public void deleteData() {
+        // start creating the query to the database
+        String createSentence = "";
+
+        try {
+            // create the connection and sql statement, then execute the query
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:.\\donations.db");
+            Statement statement = conn.createStatement();
+            ResultSet deleteResults = statement.executeQuery(getSelectQuery());
+
+            // after getting result set, loop through data using known columns retrieved from linked hash map
+            for(int i = 0; i < GrabTableData.grabTableData().get(getTable()).size(); i++) {
+                if ((i+1) != GrabTableData.grabTableData().get(getTable()).size()) {
+                    createSentence += deleteResults.getString(GrabTableData.grabTableData().get(getTable()).get(i)) + "|";
+                }
+                else {
+                    createSentence += deleteResults.getString(GrabTableData.grabTableData().get(getTable()).get(i));
+                }
+            }
+
+            // set the deleted data string and then delete the data
+            setDeletedData(createSentence);
+            statement.executeUpdate(getUserQuery());
+
+            // close the statement and connection
+            statement.close();
+            conn.close();
+        }
+        catch (SQLException sqlException) {
+            System.out.println(sqlException.getMessage());
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Data deleted:\n" + getDeletedData();
+    }
+}
--- sacredheartmission/src/Main.java
+++ sacredheartmission/src/Main.java
@@ -1,10 +1,4 @@
-import org.sqlite.SQLiteConfig;
-
-import javax.xml.transform.Result;
-import java.io.File;
-import java.sql.*;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Main {
@@ -12,12 +6,17 @@ public class Main {
 
         checkFileExistence.checkForFile();
         //GrabTableData.grabTableData();
+        DeleteData deleteData;
+
+        deleteData = new DeleteData(3, "cashDonations");
+        deleteData.deleteData();
+        System.out.println(deleteData);
 
         // use .get to get the key from linkedhashmap and to get index of arraylist
-        System.out.println(GrabTableData.grabTableData().get("cashDonations").get(0));
+        //System.out.println(GrabTableData.grabTableData().get("cashDonations").get(0));
 
         // THIS IS USED TO GRAB ALL THE KEYS AND VALUES IN HASH MAP
-        //for(Map.Entry<String, ArrayList<String>> i: tablesAndColumns.entrySet()) {
+        //for(Map.Entry<String, ArrayList<String>> i: GrabTableData.grabTableData().entrySet()) {
         //    System.out.println("KEY: " + i.getKey() + " VALUE " + i.getValue()+", ");
         //}
 
